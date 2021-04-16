833b907ba3c0bc23e36f8b00d883b5246acfbe99
mersinvald <me@mkl.dev>
2021-04-17 00:09:07 +0200

    chore: add delay in between active window queries

--- examples/watch-active-window.rs
+++ examples/watch-active-window.rs
@@ -1,12 +1,17 @@
+use std::{time::Duration, thread};
+
 fn main() {
     let mut last = None;
     loop {
         let current = active_window::active_window();
+        
         if current != last {
             last = current;
             if let Some(window) = &last {
                 println!("{:?}", window);
             }
         }
+
+        thread::sleep(Duration::from_millis(500));
     }
 }
